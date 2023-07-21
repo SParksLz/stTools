9d5bd27d5b83ed2a468b083022bb6dcfc07d6678
LiuZhen <liuzhen@51hitech.com>
2023-07-21 18:36:29 +0800

    添加house项目 修复od_test的一些问题

--- od_test.py
+++ od_test.py
@@ -33,12 +33,14 @@ for road in roads :
         new_pt = geo.createPoint()
         new_pt.setPosition((float(geometry.x), 0, float(geometry.y)))
         new_pt.setAttribValue("level", "road_geometry")
+        new_pt.setAttribValue("road_id", int(road_object.id))
         new_pt.setAttribValue("road_geo_type", geometry.desc.name)
         new_pt.setAttribValue("road_geometry_s", float(geometry.s))
         new_pt.setAttribValue("road_geometry_x", float(geometry.x))
         new_pt.setAttribValue("road_geometry_y", float(geometry.y))
         new_pt.setAttribValue("road_geometry_hdg", float(geometry.hdg))
         new_pt.setAttribValue("road_geometry_length", float(geometry.length))
+        new_pt.setAttribValue("road_length", float(road_object.length))
         if geometry.desc.name == "paramPoly3" :
             new_pt.setAttribValue("road_paramPoly_aU", float(geometry.desc.desc.aU))
             new_pt.setAttribValue("road_paramPoly_aV", float(geometry.desc.desc.aV))
@@ -59,6 +61,8 @@ for road in roads :
         center_pt = geo.createPoint()
         center_pt.setAttribValue("level", "lane_section")
         center_pt.setAttribValue("road_id", int(road_object.id))
+        center_pt.setAttribValue("road_lane_section_center_ptr_to_road_geometry", int(road_object.id))
+        center_pt.setAttribValue("road_lane_section_lane_type", "center")
         center_pt.setAttribValue("road_lane_section_s", float(current_section.s))
         center_pt.setAttribValue("road_lane_offset_s", float(current_offset.s))
         center_pt.setAttribValue("road_lane_offset_a", float(current_offset.a))
@@ -66,18 +70,26 @@ for road in roads :
         center_pt.setAttribValue("road_lane_offset_c", float(current_offset.c))
         center_pt.setAttribValue("road_lane_offset_d", float(current_offset.d))
 
-        left = current_section.left
-        right = current_section.right
+        if float(current_offset.a) != 0 and float(current_offset.b) != 0 and float(current_offset.c) != 0 and float(current_offset.d) != 0 :
+            print("{}  {}  {}  {}".format(current_offset.a, current_offset.b, current_offset.c, current_offset.d))
 
+        # left = current_section.left
+        # right = current_section.right
+        #
         # if left :
         #     left_lanes = left.lane
         #     for left_lane in left_lanes :
-                # left_pt = geo.createPoint()
-
-                # left_pt.setAttribValue("road_lane_section_rl_ptr_to_center", center_pt.number())
-                # left_pt.setAttribValue("road_lane_section_rl_lane_id", left_lane.id)
-                # left_pt.setAttribValue("road_lane_section_rl_lane_level", left_lane.level)
-                # left_pt.setAttribValue("road_lane_section_rl_lane_type", left_lane.type)
+        #         left_pt = geo.createPoint()
+        #
+        #         left_pt.setAttribValue("road_lane_section_rl_ptr_to_center", center_pt.number())
+        #         left_pt.setAttribValue("road_lane_section_rl_lane_id", left_lane.id)
+        #         left_pt.setAttribValue("road_lane_section_rl_lane_level", left_lane.level)
+        #         left_pt.setAttribValue("road_lane_section_rl_lane_type", left_lane.type)
+        #         left_pt.setAttribValue("level", "lane_section_left")
+        #
+        #         border = left_lane.border
+        #         height = left_lane.height
+        #         width  = left_lane.width
                 # left_pt.setAttribValue("road_lane_section_rl_lane_border", float(left_lane.border))
                 # left_pt.setAttribValue("road_lane_section_rl_lane_height", float(left_lane.height))
                 # left_pt.setAttribValue("road_lane_section_rl_lane_width", float(left_lane.width))
